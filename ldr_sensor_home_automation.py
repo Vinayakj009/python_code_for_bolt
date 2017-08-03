d11d10066a430564bfb019e3b4925f84489f6ade
Datta_TECH <datta.naik@inventrom.com>
2017-08-03 16:18:43 +0530

    Code to automate Home lights based on digital LDR sensor

---
+++ ldr_sensor_home_automation.py
@@ -0,0 +1,48 @@
+import requests,json
+import time
+
+ON = 1
+OFF = 0
+status_flag = OFF
+
+def switch_on():
+        r = requests.get("http://cloud.boltiot.com/remote/api_key/digitalMultiWrite?pins=1,2&states=HIGH,HIGH&"+
+                         "deviceName=BOLTXXXXXX")
+        print "On"
+
+def switch_off():
+        r = requests.get("http://cloud.boltiot.com/remote/api_key/digitalMultiWrite?pins=1,2&states=LOW,LOW&"+
+                 "deviceName=BOLTXXXXXX")
+        print "Off"
+
+def get_ldr_data():
+        r = requests.get("http://cloud.boltiot.com/remote/api_key/analogRead?pin=A0&"+
+                         "deviceName=BOLTYYYYYY")
+        data = json.loads(r.text)
+        print "ldr adc value = ", data['value']
+        return data['value']
+
+switch_off()
+
+#note the code below make sure the LDR is not in the same room as the Bulb
+#it is ideal that the LDR monitors sunlight from a diff Bolt
+#Bulb should be connected to a diff Bolt in the room
+#If both are in the same room the code will possibly make the Bulb toggle every 5 secs
+while True:
+        try:
+            ldr_data = int(get_ldr_data())
+        if ldr_data == 0 and status_flag == OFF: #Low light, LOW on pin 4
+            switch_on()
+            status_flag = ON
+            print "status",status_flag
+        elif ldr_data == 1 and status_flag == ON: #Bright light , HIGH on pin 4
+            switch_off()
+            status_flag = OFF
+                        print "status",status_flag
+        else:
+             pass
+        time.sleep(5)
+        except Exception as e:
+                print e
+                time.sleep(5)
+
