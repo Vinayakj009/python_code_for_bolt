ba9fe88f7239a3eef6979a9c0645b48ca387df75
Vinayak Joshi <Vinayakj009@gmail.com>
2017-09-16 09:25:44 +0530

    Update digital_sensor_trigger_mail.py
    Hi, I made these changes as  most coders will probably copy and paste the code instead of actually reading the comments. 
    When they face an error, its easier to to tell them to change the variables at start of the code, then look through the whole code.

--- digital_sensor_trigger_mail.py
+++ digital_sensor_trigger_mail.py
@@ -1,8 +1,10 @@
 import requests, time
 
-key='YOUR API KEY HERE'
+key='PLACE YOUR MAILGUN API KEY HERE'
 sandbox='YOUR SANDBOX URL HERE'
 recipient='YOUR EMAIL HERE' #you can only send mails to yourself
+bolt_api_key='PLACE YOUR BOLTCLOUD API KEY HERE' #This api key can be found in API tab of bolt cloud login
+bolt_id='PLACE YOUR BOLT DEVICE ID HERE' #example BOLT345678. This device id is visible in you boltcloud login devices tab, or products tab
 # TO send mail to other users you must first verify the user from mailgun website
 
 def send_mail(body):
@@ -17,8 +19,7 @@ def send_mail(body):
     print'Body:   {0}'.format(request.text)
 
 while True:
-    #replace api_key and BOLT id in the statement below with you Bolt Cloud API key and Bolt hardware ID
-    r = requests.get('http://cloud.boltiot.com/remote/api_key/digitalRead?pin=4&deviceName=BOLTXXXXXX')
+    r = requests.get('http://cloud.boltiot.com/remote/'+bolt_api_key+'/digitalRead?pin=4&deviceName='+bolt_id)
     data = json.loads(r.text)
     try:
         print data['value']
